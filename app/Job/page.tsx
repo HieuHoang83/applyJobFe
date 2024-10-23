451e5f1a70bf2879a4c4b640c1b504b56413ac2f
Hieu Hoang <hoangminhhieu271219@gmail.com>
2024-10-23 14:11:37 +0700

    merge code

--- app/Job/page.tsx
+++ app/Job/page.tsx
@@ -1,20 +1,27 @@
-"use client"
-import SearchBar from '@/components/Search/SearchBar'
-import React from 'react'
+"use client";
+import JobCard from "@/components/Folder Components/JobCard/JobCard";
+import SearchBar from "@/components/Search/SearchBar";
+import React from "react";
 
 const JobPage = () => {
-    return (
-           
-           <div className='bg-custom h-[100vh] '>
-                <div className=" mx-[15%]  pt-[60px]">
-                    <SearchBar></SearchBar>
-                </div>
-                <div >
+  return (
+    <div className=" h-[100vh] ">
+      <div className=" mx-[15%]  pt-[60px]">
+        <SearchBar></SearchBar>
+      </div>
 
-                </div>
-            </div>
-           
-    )
-}
+      <div className="text-center mt-[50px] mx-[12%] gap-4  grid grid-cols-3 ">
+        <JobCard />
+        <JobCard />
+        <JobCard />
+        <JobCard />
+        <JobCard />
+        <JobCard />
+        <JobCard />
+        <JobCard />
+      </div>
+    </div>
+  );
+};
 
-export default JobPage
+export default JobPage;
