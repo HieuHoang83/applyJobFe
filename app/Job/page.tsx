54e9802818d1c362140e07285830ddf302e489b6
Hieu Hoang <hoangminhhieu271219@gmail.com>
2024-10-23 14:54:57 +0700

    ok button switch

--- app/Job/page.tsx
+++ app/Job/page.tsx
@@ -1,16 +1,20 @@
 "use client";
 import JobCard from "@/components/Folder Components/JobCard/JobCard";
 import SearchBar from "@/components/Search/SearchBar";
+import SwitchBtn from "@/components/switch/switchBtn";
 import React from "react";
 
 const JobPage = () => {
+  let x: number = 0;
   return (
     <div className=" h-[100vh] ">
       <div className=" mx-[15%]  pt-[60px]">
         <SearchBar></SearchBar>
       </div>
-
-      <div className="text-center mt-[50px] mx-[12%] gap-4  grid grid-cols-3 ">
+      <div>
+        <SwitchBtn></SwitchBtn>
+      </div>
+      <div className="text-center mt-[50px] mx-[12%] gap-[50px]  grid grid-cols-3 ">
         <JobCard />
         <JobCard />
         <JobCard />
