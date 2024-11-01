da6b36d6ba6ad8055405173045a0f1eee8a2d8c6
Hieu Hoang <hoangminhhieu271219@gmail.com>
2024-11-01 22:58:12 +0700

    ok

--- app/resuitment/page.tsx
+++ app/resuitment/page.tsx
@@ -7,7 +7,10 @@ import { Calendar } from "@/components/ui/calendar";
 import { Input } from "@/components/ui/input";
 import React, { useEffect, useRef } from "react";
 import { FcSearch } from "react-icons/fc";
+import { GiTakeMyMoney } from "react-icons/gi";
 import { IoSearchSharp } from "react-icons/io5";
+import { RiMapPin2Line } from "react-icons/ri";
+import { TfiTime } from "react-icons/tfi";
 
 let dtoBđ = {
   title: "",
@@ -28,9 +31,12 @@ function ResuitmentPage() {
   `;
   const titleRef = useRef<HTMLTextAreaElement>(null);
   const descriptitonRef = useRef<HTMLTextAreaElement>(null);
-  const addresssRef = useRef<HTMLTextAreaElement>(null);
+  const addresssRef = useRef<HTMLSelectElement>(null);
+  const salaryRef = useRef<HTMLSelectElement>(null);
+  const experienceRef = useRef<HTMLSelectElement>(null);
+
   const requireRef = useRef<HTMLTextAreaElement>(null);
-  const salaryRef = useRef<HTMLTextAreaElement>(null);
+
   const offerRef = useRef<HTMLTextAreaElement>(null);
   const timeWorkRef = useRef<HTMLTextAreaElement>(null);
   const startRef = useRef<HTMLInputElement>(null);
@@ -50,11 +56,11 @@ function ResuitmentPage() {
       setErr("Vui lòng nhập đủ thông tin");
     else {
       //@ts-ignore
-      dtoBđ.address = addresssRef.current?.value;
+      //dtoBđ.address = addresssRef.current?.value;
       //@ts-ignore
       dtoBđ.require = requireRef.current?.value;
       //@ts-ignore
-      dtoBđ.salary = salaryRef.current?.value;
+      //dtoBđ.salary = salaryRef.current?.value;
       //@ts-ignore
       dtoBđ.offer = offerRef.current?.value;
       //@ts-ignore
@@ -73,7 +79,54 @@ function ResuitmentPage() {
     }
     //@ts-ignore
   };
+  let [listExperience] = React.useState<any>([
+    {
+      value: "Không yêu cầu kinh nghiệm",
+    },
+    {
+      value: "dưới 1 năm",
+    },
+    {
+      value: "Trên 1 năm",
+    },
+    {
+      value: "Trên 2 năm",
+    },
+    {
+      value: "Trên 3 năm",
+    },
+    {
+      value: "Trên 4 năm",
+    },
+    {
+      value: "Trên 5 năm",
+    },
+  ]);
+  let [ListAddress] = React.useState<any>([
+    {
+      value: "Không yêu cầu kinh nghiệm",
+    },
+    {
+      value: "dưới 1 năm",
+    },
+    {
+      value: "Trên 1 năm",
+    },
+    {
+      value: "Trên 2 năm",
+    },
+    {
+      value: "Trên 3 năm",
+    },
+    {
+      value: "Trên 4 năm",
+    },
+    {
+      value: "Trên 5 năm",
+    },
+  ]);
 
+  let selectref = useRef();
   const Body = () => {
     return (
       <div className="mx-[18%] mt-20 px-14 py-10  bg-white">
@@ -83,6 +136,63 @@ function ResuitmentPage() {
           placeholder="Nhập tiêu đề của bài tuyển dụng"
           className="mt-3 flex min-h-[60px] w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300"
         />
+        <div className="flex mt-3 text-lg h-[120px] ">
+          <div className="flex flex-row mt-3 justify-start   ">
+            <div className="flex mt-1 flex-col ">
+              <div>Mức lương</div>
+              <select id="selection" className="border p-1 rounded-md ">
+                <option value="" key="0">
+                  Mức lương
+                </option>
+                {listExperience.map((item: any, index: any) => {
+                  return (
+                    <option value={item.value} key={index} id={item.value}>
+                      {item.value}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+          </div>
+          <div className="flex flex-row mt-3 justify-start   ">
+            <div className="flex mt-1 flex-col ml-4 ">
+              <div>Địa điểm</div>
+              <select
+                id="selection"
+                className="border p-1 rounded-md w-[150px]"
+                ref={addresssRef}
+              >
+                <option value="" key="0">
+                  Địa điểm
+                </option>
+                {ListAddress.map((item: any, index: any) => {
+                  return (
+                    <option value={item.value} key={index} id={item.value}>
+                      {item.value}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+          </div>
+          <div className="flex flex-row mt-3 justify-start   ">
+            <div className="flex mt-1 flex-col ml-4  ">
+              <div> Kinh Nghiệm </div>
+              <select id="selection" className="border p-1 rounded-md">
+                <option value="" key="0">
+                  Kinh nghiệm tối thiểu
+                </option>
+                {listExperience.map((item: any, index: any) => {
+                  return (
+                    <option value={item.value} key={index} id={item.value}>
+                      {item.value}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+          </div>
+        </div>
 
         <div className="mt-5 text-xl">Mô tả công việc</div>
         <textarea
@@ -90,24 +200,14 @@ function ResuitmentPage() {
           placeholder={Hd}
           className="mt-3 flex min-h-[100px] w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300"
         />
-        <div className="mt-5 text-xl">Địa điểm</div>
-        <textarea
-          ref={addresssRef}
-          placeholder={Hd}
-          className="mt-3 flex min-h-[100px] w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300"
-        />
+
         <div className="mt-5 text-xl">Yêu cầu ứng viên</div>
         <textarea
           ref={requireRef}
           placeholder={Hd}
           className="mt-3 flex min-h-[100px] w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300"
         />
-        <div className="mt-5 text-xl">Lương</div>
-        <textarea
-          ref={salaryRef}
-          placeholder={Hd}
-          className="mt-3 flex min-h-[100px] w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300"
-        />
+
         <div className="mt-5 text-xl">Quyền lợi</div>
         <textarea
           ref={offerRef}
